dce76a09b231fdcd9e74a433c42c360565a2758c
Evan Boswell <35876833+Manectric22@users.noreply.github.com>
2019-08-26 09:48:38 -0400

    evan was in da hallway so i pushin

--- wwwroot/js/Script1.js
+++ wwwroot/js/Script1.js
@@ -1,1 +1,5 @@
-// JavaScript source code
+const getTeams = () => {
+    function fetch('https://www.thebluealliance.com/api/v3/events/{2015}')
+        .then(res => res.json())
+        .then(teams => console.log(teams))
+}
